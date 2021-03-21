72611c1e86bd3a33c534cc044a60582feb9a3bd9
Wes PC <wesleyoliveirap@gmail.com>
2021-03-21 13:48:05 -0300

    First commit; User and Auth Module, Controller and Service

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,12 @@
+import { Body, Controller, Post } from '@nestjs/common';
+import { UserService } from 'src/user/user.service';
+
+@Controller()
+export class AuthController {
+    constructor (private userService: UserService) {}
+
+    @Post('register')
+    async register(@Body() body) {
+        return this.userService.create(body);
+    }
+}
