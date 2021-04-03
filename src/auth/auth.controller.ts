7e92fecb9561390291e66ead49b7cc393063e0e3
wesoz <wesleyop@hotmail.com>
2021-04-02 21:01:16 -0300

    Added logout function

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -43,7 +43,7 @@ export class AuthController {
   async login(
     @Body('email') email: string,
     @Body('password') password: string,
-    @Res() response: Response,
+    @Res({ passthrough: true }) response: Response,
   ) {
     const user = await this.userService.findOne({ email });
 
@@ -67,5 +67,12 @@ export class AuthController {
     const data = await this.jwtService.verifyAsync(cookie);
 
     return this.userService.findOne({ id: data['id'] });
+  }
+
+  @Post('logout')
+  async logout(@Res({ passthrough: true }) response: Response) {
+    response.clearCookie('jwt');
+
+    return { message: 'Success' };
   }
 }
